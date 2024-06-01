371ac1aaca6f909e2a87b224d61a4fc3f472da7a
João Gabriel Oliveira <30786108+jg-lopes@users.noreply.github.com>
2024-06-01 15:26:06 +0100

    more people.

--- src/main.js
+++ src/main.js
@@ -7,13 +7,23 @@ import lauren_004 from './assets/lauren_004.jpg'
 import julia_005 from './assets/julia_005.jpg'
 import luke_006 from './assets/luke_006.jpg'
 import aivis_007 from './assets/aivis_007.jpg'
+import john_008 from './assets/john_008.jpg'
+import black_square from './assets/black_square.jpeg'
+import axelle_010 from './assets/010_axelle.jpg'
+import natalia_011 from './assets/011_natalia.jpg'
+import charlie_015 from './assets/015_charlie.jpg'
+import dan_016 from './assets/016_dan.jpg'
+import maria_017 from './assets/017_maria.jpg'
+import sienna_018 from './assets/018_sienna.jpg'
+import wagstaff_019 from './assets/019_wagstaff.jpg'
+
 
 const playerGrid = document.getElementById("hexGrid");
 
 const row1 = [ben_001, tom_002, ananya_003, lauren_004, julia_005, luke_006]
-const row2 = [aivis_007, black_square, black_square, black_square, black_square]
-const row3 = [black_square, black_square, black_square, black_square, black_square, black_square]
-const row4 = [black_square, black_square, black_square, black_square, black_square]
+const row2 = [aivis_007, john_008, black_square, axelle_010, natalia_011]  // 7-11
+const row3 = [black_square, black_square, black_square, charlie_015, dan_016, maria_017] // 12-17
+const row4 = [sienna_018, wagstaff_019, black_square, black_square, black_square] // 18-22
 const row5 = [black_square, black_square, black_square, black_square, black_square, black_square]
 const row6 = [black_square, black_square, black_square, black_square, black_square]
 
